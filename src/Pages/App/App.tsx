07b3a5a327aa5d9b9c7f7bc8dc14df1e0b77a2ad
OskarKocjan <oskark99@interia.pl>
2023-07-02 12:31:00 +0200

    cleanup

--- src/Pages/App/App.tsx
+++ src/Pages/App/App.tsx
@@ -1,18 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { completeAnimeDataset } from "../../Curator/getNewSeasons";
 
 function App() {
-  const [newAnimeData, setNewAnimeData] = useState({});
-
-  // useEffect(() => {
-  //   completeAnimeDataset("spring", 2021, 1)
-  //     .then((data) => {
-  //       setNewAnimeData(data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
   return (
     <div className='App'>
       <h1>HELOOOOOOOOOOOOOOOOO</h1>
